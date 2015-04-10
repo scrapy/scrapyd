b670f5e7fe0c84c651d50d5ce9f4a8cabde27c39
Łukasz Bołdys <mail@utek.pl>
2015-04-10 07:57:43 +0200

    moved scripts from bin to scrapyd/scripts and added entry_points with console_scripts to setup.py

--- scrapyd/scripts/scrapyd_run.py
+++ scrapyd/scripts/scrapyd_run.py
@@ -4,5 +4,8 @@ from twisted.scripts.twistd import run
 from os.path import join, dirname
 from sys import argv
 import scrapyd
-argv[1:1] = ['-n', '-y', join(dirname(scrapyd.__file__), 'txapp.py')]
-run()
+
+
+def main():
+    argv[1:1] = ['-n', '-y', join(dirname(scrapyd.__file__), 'txapp.py')]
+    run()
