d1e6f5a620dc421e7d03ae0e02ef71e47300ef84
Paul Tremberth <paul.tremberth@gmail.com>
2016-07-20 20:28:51 +0200

    PY3 Depend on six

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ except ImportError:
 
 from os.path import join, dirname
 
-if sys.version_info[:2] != (2, 7):
-    sys.exit('Python 2.7 is required for scrapyd')
-
 with open(join(dirname(__file__), 'scrapyd/VERSION')) as f:
     version = f.read().strip()
 
@@ -42,7 +39,7 @@ setup_args = {
 
 
 if using_setuptools:
-    setup_args['install_requires'] = ['Twisted>=8.0', 'Scrapy>=0.17']
+    setup_args['install_requires'] = ['Twisted>=8.0', 'Scrapy>=0.17', 'six']
     setup_args['entry_points'] = {'console_scripts': [
         'scrapyd = scrapyd.scripts.scrapyd_run:main'
     ]}
