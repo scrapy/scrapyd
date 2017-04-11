3f9449588cffc113b1fabd851728e23266e222b7
Νικόλαος-Διγενής Καραγιάννης <digenishjkl@gmail.com>
2017-04-11 16:29:00 +0300

    refactoring

--- scrapyd/_deprecate.py
+++ scrapyd/_deprecate.py
@@ -26,14 +26,9 @@ class WarningMeta(type):
 
 
 def deprecate_class(cls):
-    base_metaclasses = set(type(b) for b in cls.__bases__) | {WarningMeta}
     class WarningMeta2(WarningMeta):
         pass
-    for b in base_metaclasses:
-        if b not in WarningMeta2.__bases__:
-            WarningMeta2.__bases__ += (b,)
-    #WarningMeta2 = type('WarningMeta', tuple(base_metaclasses), {})
-
-    class cls2(cls):
-        __metaclass__ = WarningMeta2
-    return cls2
+    for b in cls.__bases__:
+        if type(b) not in WarningMeta2.__bases__:
+            WarningMeta2.__bases__ += (type(b),)
+    return WarningMeta2(cls.__name__, (cls,), {})
