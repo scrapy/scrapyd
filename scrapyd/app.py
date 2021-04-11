788ee1b98006721103115678c0ac6ded769675b1
LxL <my8100@gmail.com>
2021-04-11 18:03:35 +0800

    Move basic auth wrapping code to function create_wrapped_resource (#337)
    * Move all the basic auth wrapping code from the app to the website module
    
    * Move the get_resource function into the app module
    
    * Rename func create_wrapped_resource in the app module

--- scrapyd/app.py
+++ scrapyd/app.py
@@ -1,5 +1,6 @@
 import sys
 
+from scrapy.utils.misc import load_object
 from twisted.application.service import Application
 from twisted.application.internet import TimerService, TCPServer
 from twisted.web import server
@@ -7,15 +8,31 @@ from twisted.python import log
 from twisted.cred.portal import Portal
 from twisted.web.guard import HTTPAuthSessionWrapper, BasicCredentialFactory
 
-from scrapy.utils.misc import load_object
-
 from .interfaces import IEggStorage, IPoller, ISpiderScheduler, IEnvironment
 from .eggstorage import FilesystemEggStorage
 from .scheduler import SpiderScheduler
 from .poller import QueuePoller
 from .environ import Environment
-from .config import Config
 from .basicauth import PublicHTMLRealm, StringCredentialsChecker
+
+
+def create_wrapped_resource(webcls, config, app):
+    username = config.get('username', '')
+    password = config.get('password', '')
+    if ':' in username:
+        sys.exit("The `username` option contains illegal character ':', "
+                 "check and update the configuration file of Scrapyd")
+    resource = webcls(config, app)
+    if username and password:
+        log.msg("Basic authentication enabled")
+        portal = Portal(PublicHTMLRealm(resource),
+                        [StringCredentialsChecker(username, password)])
+        credential_factory = BasicCredentialFactory("Auth")
+        return HTTPAuthSessionWrapper(portal, [credential_factory])
+    else:
+        log.msg("Basic authentication disabled as either `username` or `password` is unset")
+        return resource
+
 
 def application(config):
     app = Application("Scrapyd")
@@ -41,21 +58,7 @@ def application(config):
 
     webpath = config.get('webroot', 'scrapyd.website.Root')
     webcls = load_object(webpath)
-
-    username = config.get('username', '')
-    password = config.get('password', '')
-    if username and password:
-        if ':' in username:
-            sys.exit("The `username` option contains illegal character ':', "
-                     "check and update the configuration file of Scrapyd")
-        portal = Portal(PublicHTMLRealm(webcls(config, app)),
-                        [StringCredentialsChecker(username, password)])
-        credential_factory = BasicCredentialFactory("Auth")
-        resource = HTTPAuthSessionWrapper(portal, [credential_factory])
-        log.msg("Basic authentication enabled")
-    else:
-        resource = webcls(config, app)
-        log.msg("Basic authentication disabled as either `username` or `password` is unset")
+    resource = create_wrapped_resource(webcls, config, app)
     webservice = TCPServer(http_port, server.Site(resource), interface=bind_address)
     log.msg(format="Scrapyd web console available at http://%(bind_address)s:%(http_port)s/",
             bind_address=bind_address, http_port=http_port)
