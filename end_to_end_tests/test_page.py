a2834ebd1205bfcb4cef00fd222ab9dba1bd52ac
LxL <my8100@gmail.com>
2021-04-20 23:13:11 +0800

    Add files via upload

---
+++ end_to_end_tests/test_page.py
@@ -0,0 +1,9 @@
+from end_to_end_tests.utils import BASE_URL, TRUE_AUTH, get_url, get
+
+
+def test_homepage():
+    r = get(BASE_URL, auth=TRUE_AUTH)
+
+def test_pages():
+    for page in ['/jobs']:  # '/logs'
+        r = get(get_url(page), auth=TRUE_AUTH)
