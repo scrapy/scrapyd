c72c3e6d4180e35202ac246f74f72962f90ff1d0
Pawel Miech <pawelmhm@gmail.com>
2021-11-10 21:03:08 +0100

    [mock_app] better way of setting http_port and auth
    needed to handle windows edge cases

--- scrapyd/tests/start_mock_app.py
+++ scrapyd/tests/start_mock_app.py
@@ -1,29 +1,29 @@
 import sys
 import tempfile
+from configparser import ConfigParser
+from os import path
 
 from twisted.application import app
 from twisted.internet import reactor
 from twisted.python import log
 
-from scrapyd import get_application, Config
+from scrapyd import Config
+from scrapyd.app import application
 
 
 def _get_config(http_port=None, authentication=None):
+    scrapyd_config = Config()
     if http_port is None:
-        http_port = "6800"
-    scrapyd_conf = f"""
-[scrapyd]
-http_port = {str(http_port)}
-    """
+        http_port = 6800
+
+    section = 'scrapyd'
+    scrapyd_config.cp.set(section, 'http_port', str(http_port))
+
     if authentication is not None:
         username, password = authentication.split(":")
-        scrapyd_conf += "\n"
-        scrapyd_conf += "username = " + username + "\n"
-        scrapyd_conf += "password = " + password + "\n"
-    tmp_file = tempfile.NamedTemporaryFile()
-    tmp_file.write(scrapyd_conf.encode())
-    tmp_file.seek(0)
-    return Config(extra_sources=[tmp_file.name])
+        scrapyd_config.cp.set(section, 'username', username)
+        scrapyd_config.cp.set(section, 'password', password)
+    return scrapyd_config
 
 
 if __name__ == "__main__":
@@ -31,6 +31,6 @@ if __name__ == "__main__":
     http_port = int(sys.argv[1])
     authentication = sys.argv[2] if len(sys.argv) == 3 else None
     conf = _get_config(http_port=http_port, authentication=authentication)
-    application = get_application(config=conf)
+    application = application(config=conf)
     app.startApplication(application, False)
     reactor.run()
