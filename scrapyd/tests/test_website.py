5ebe73407141c78f6c86ceb5e1d8bb8476194e8e
mojtaba asadi <arian.4339@gmail.com>
2022-01-01 16:24:20 +0330

    changed initial html according twisted template output in test

--- scrapyd/tests/test_website.py
+++ scrapyd/tests/test_website.py
@@ -32,7 +32,11 @@ class TestWebsite:
         }
         headers = txrequest.responseHeaders.getAllRawHeaders()
         assert dict(headers) == expect_headers
-        initial = '<html xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1">'
+        initial = '''<html>
+
+<head>
+    <title>Scrapyd</title>
+</head>'''
         assert content.decode().startswith(initial)
 
     def test_render_home(self, txrequest, scrapyd_site):
