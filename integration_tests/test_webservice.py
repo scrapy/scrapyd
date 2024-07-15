38367ba013cf4b5f184c79169947fece65b839d5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-15 15:36:17 -0400

    test: Add more integration tests

--- integration_tests/test_webservice.py
+++ integration_tests/test_webservice.py
@@ -1,3 +1,5 @@
+import scrapy
+
 from integration_tests import req
 
 
@@ -9,11 +11,36 @@ def assert_webservice(method, path, expected, **kwargs):
     assert json == expected
 
 
-def test_listjobs():
+def test_daemonstatus():
     assert_webservice(
         "get",
-        "/listjobs.json",
-        {"status": "ok", "pending": [], "running": [], "finished": []},
+        "/daemonstatus.json",
+        {"status": "ok", "running": 0, "pending": 0, "finished": 0}
+    )
+
+
+def test_schedule():
+    assert_webservice(
+        "post",
+        "/schedule.json",
+        {
+            "status": "error",
+            "message": (
+                f'Scrapy {scrapy.__version__} - no active project\n\n'
+                'Unknown command: list\n\n'
+                'Use "scrapy" to see available commands\n'
+            ),
+        },
+        data={"project": "nonexistent", "spider": "nospider"},
+    )
+
+
+def test_cancel_nonexistent():
+    assert_webservice(
+        "post",
+        "/cancel.json",
+        {"status": "error", "message": "'nonexistent'"},
+        data={"project": "nonexistent", "job": "nojob"},
     )
 
 
@@ -22,6 +49,48 @@ def test_listprojects():
         "get",
         "/listprojects.json",
         {"status": "ok", "projects": []},
+    )
+
+
+def test_listversions():
+    assert_webservice(
+        "get",
+        "/listversions.json",
+        {"status": "ok", "versions": []},
+        params={"project": "sample"},
+    )
+
+
+def test_listspiders_nonexistent():
+    assert_webservice(
+        "get",
+        "/listspiders.json",
+        {
+            "status": "error",
+            "message": (
+                f'Scrapy {scrapy.__version__} - no active project\n\n'
+                'Unknown command: list\n\n'
+                'Use "scrapy" to see available commands\n'
+            ),
+        },
+        params={"project": "nonexistent"},
+    )
+
+
+def test_listjobs():
+    assert_webservice(
+        "get",
+        "/listjobs.json",
+        {"status": "ok", "pending": [], "running": [], "finished": []},
+    )
+
+
+def test_delversion_nonexistent():
+    assert_webservice(
+        "post",
+        "/delversion.json",
+        {"status": "error", "message": "[Errno 2] No such file or directory: 'eggs/nonexistent/noegg.egg'"},
+        data={"project": "nonexistent", "version": "noegg"},
     )
 
 
