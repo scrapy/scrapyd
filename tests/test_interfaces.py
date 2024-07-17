a16f98aaf2bd3a11cf9f67e0b7704de293f01406
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-17 17:53:22 -0400

    test: Verify interface implementations. Remove TODO from cd35bd4c.

---
+++ tests/test_interfaces.py
@@ -0,0 +1,28 @@
+import pytest
+from twisted.cred.checkers import ICredentialsChecker
+from twisted.cred.portal import IRealm
+from zope.interface.verify import verifyClass
+
+from scrapyd.basicauth import PublicHTMLRealm, StringCredentialsChecker
+from scrapyd.eggstorage import FilesystemEggStorage
+from scrapyd.environ import Environment
+from scrapyd.interfaces import IEggStorage, IEnvironment, IJobStorage, IPoller, ISpiderQueue, ISpiderScheduler
+from scrapyd.jobstorage import MemoryJobStorage, SqliteJobStorage
+from scrapyd.poller import QueuePoller
+from scrapyd.scheduler import SpiderScheduler
+from scrapyd.spiderqueue import SqliteSpiderQueue
+
+
+@pytest.mark.parametrize('cls,interface', [
+    (PublicHTMLRealm, IRealm),
+    (StringCredentialsChecker, ICredentialsChecker),
+    (FilesystemEggStorage, IEggStorage),
+    (Environment, IEnvironment),
+    (MemoryJobStorage, IJobStorage),
+    (SqliteJobStorage, IJobStorage),
+    (QueuePoller, IPoller),
+    (SpiderScheduler, ISpiderScheduler),
+    (SqliteSpiderQueue, ISpiderQueue),
+])
+def test_interface(cls, interface):
+    verifyClass(interface, cls)
