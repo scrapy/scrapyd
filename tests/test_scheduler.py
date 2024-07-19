b146fff4c41327a41221d9e0229e4e8112285f6a
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-19 04:24:58 -0400

    tests: Remove classes from tests (scheduler)

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,6 +1,6 @@
 import os
 
-from twisted.trial import unittest
+import pytest
 from zope.interface.verify import verifyObject
 
 from scrapyd.config import Config
@@ -9,40 +9,39 @@ from scrapyd.scheduler import SpiderScheduler
 from scrapyd.utils import get_spider_queues
 
 
-class SpiderSchedulerTest(unittest.TestCase):
-    def setUp(self):
-        d = self.mktemp()
-        eggs_dir = self.eggs_dir = os.path.join(d, "eggs")
-        dbs_dir = os.path.join(d, "dbs")
-        os.mkdir(d)
-        os.makedirs(eggs_dir)
-        os.makedirs(dbs_dir)
-        os.makedirs(os.path.join(eggs_dir, "mybot1"))
-        os.makedirs(os.path.join(eggs_dir, "mybot2"))
-        config = Config(values={"eggs_dir": eggs_dir, "dbs_dir": dbs_dir})
-        self.queues = get_spider_queues(config)
-        self.sched = SpiderScheduler(config)
+@pytest.fixture()
+def scheduler(tmpdir):
+    eggs_dir = os.path.join(tmpdir, "eggs")
+    dbs_dir = os.path.join(tmpdir, "dbs")
+    os.makedirs(os.path.join(eggs_dir, "mybot1"))
+    os.makedirs(os.path.join(eggs_dir, "mybot2"))
+    config = Config(values={"eggs_dir": eggs_dir, "dbs_dir": dbs_dir})
+    return SpiderScheduler(config)
 
-    def test_interface(self):
-        verifyObject(ISpiderScheduler, self.sched)
 
-    def test_list_update_projects(self):
-        self.assertEqual(sorted(self.sched.list_projects()), sorted(["mybot1", "mybot2"]))
+def test_interface(scheduler):
+    verifyObject(ISpiderScheduler, scheduler)
 
-        os.makedirs(os.path.join(self.eggs_dir, "mybot3"))
-        self.sched.update_projects()
 
-        self.assertEqual(sorted(self.sched.list_projects()), sorted(["mybot1", "mybot2", "mybot3"]))
+def test_list_update_projects(scheduler):
+    assert sorted(scheduler.list_projects()) == sorted(["mybot1", "mybot2"])
 
-    def test_schedule(self):
-        q1, q2 = self.queues["mybot1"], self.queues["mybot2"]
+    os.makedirs(os.path.join(scheduler.config.get("eggs_dir"), "mybot3"))
+    scheduler.update_projects()
 
-        self.assertFalse(q1.count())
+    assert sorted(scheduler.list_projects()) == sorted(["mybot1", "mybot2", "mybot3"])
 
-        self.sched.schedule("mybot1", "myspider1", 2, a="b")
-        self.sched.schedule("mybot2", "myspider2", 1, c="d")
-        self.sched.schedule("mybot2", "myspider3", 10, e="f")
 
-        self.assertEqual(q1.pop(), {"name": "myspider1", "a": "b"})
-        self.assertEqual(q2.pop(), {"name": "myspider3", "e": "f"})
-        self.assertEqual(q2.pop(), {"name": "myspider2", "c": "d"})
+def test_schedule(scheduler):
+    queues = get_spider_queues(scheduler.config)
+    q1, q2 = queues["mybot1"], queues["mybot2"]
+
+    assert not q1.count()
+
+    scheduler.schedule("mybot1", "myspider1", 2, a="b")
+    scheduler.schedule("mybot2", "myspider2", 1, c="d")
+    scheduler.schedule("mybot2", "myspider3", 10, e="f")
+
+    assert q1.pop() == {"name": "myspider1", "a": "b"}
+    assert q2.pop() == {"name": "myspider3", "e": "f"}
+    assert q2.pop() == {"name": "myspider2", "c": "d"}
