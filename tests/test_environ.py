71e3d403cce867003de2f6a86c232cf394b78615
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-19 18:06:27 -0400

    test: Update tests for "Remove undocumented and unused internal environment variables" 42ebd05

--- tests/test_environ.py
+++ tests/test_environ.py
@@ -9,7 +9,6 @@ from scrapyd.exceptions import DirectoryTraversalError
 from scrapyd.interfaces import IEnvironment
 
 msg = {"_project": "mybot", "_spider": "myspider", "_job": "ID"}
-slot = 3
 
 
 @pytest.fixture()
@@ -25,29 +24,10 @@ def test_interface(environ):
 
 
 def test_get_environment_with_eggfile(environ):
-    env = environ.get_environment(msg, slot)
+    env = environ.get_environment(msg, 3)
 
     assert env["SCRAPY_PROJECT"] == "mybot"
-    assert env["SCRAPYD_SLOT"] == "3"
-    assert env["SCRAPYD_SPIDER"] == "myspider"
-    assert env["SCRAPYD_JOB"] == "ID"
-    assert env["SCRAPYD_LOG_FILE"].endswith(os.path.join("mybot", "myspider", "ID.log"))
-    if env.get("SCRAPYD_FEED_URI"):  # Not compulsory
-        assert env["SCRAPYD_FEED_URI"].startswith(f"file://{os.getcwd()}")
-        assert env["SCRAPYD_FEED_URI"].endswith(os.path.join("mybot", "myspider", "ID.jl"))
     assert "SCRAPY_SETTINGS_MODULE" not in env
-
-
-def test_get_environment_with_no_items_dir():
-    config = Config(values={"items_dir": "", "logs_dir": ""})
-    config.cp.add_section("settings")
-    config.cp.set("settings", "newbot", "newbot.settings")
-
-    environ = Environment(config, initenv={})
-    env = environ.get_environment(msg, slot)
-
-    assert "SCRAPYD_FEED_URI" not in env
-    assert "SCRAPYD_LOG_FILE" not in env
 
 
 @pytest.mark.parametrize("values", [{"items_dir": "../items"}, {"logs_dir": "../logs"}])
@@ -57,7 +37,7 @@ def test_get_environment_secure(values, key, value):
     environ = Environment(config, initenv={})
 
     with pytest.raises(DirectoryTraversalError) as exc:
-        environ.get_environment({**msg, key: value}, slot)
+        environ.get_settings({**msg, key: value})
 
     assert str(exc.value) == (
         f"{value if key == '_project' else 'mybot'}{os.sep}"
