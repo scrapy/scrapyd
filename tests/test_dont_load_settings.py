e3a35f6f69eb95968c27e538b94aadad47966fc8
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-19 03:14:15 -0400

    tests: Remove classes from tests (dont_load_settings)

--- tests/test_dont_load_settings.py
+++ tests/test_dont_load_settings.py
@@ -1,25 +1,16 @@
 import sys
-import unittest
+
+SETTINGS_SAFE_MODULES = (
+    "scrapy.utils.project",
+    "scrapy.utils.conf",
+    "scrapyd.interfaces",
+    "scrapyd.runner",
+)
 
 
-class SettingsSafeModulesTest(unittest.TestCase):
-    # these modules must not load scrapy.conf
-    SETTINGS_SAFE_MODULES = (
-        "scrapy.utils.project",
-        "scrapy.utils.conf",
-        "scrapyd.interfaces",
-        "scrapyd.runner",
-    )
+def test_modules_that_shouldnt_load_settings():
+    sys.modules.pop("scrapy.conf", None)
+    for m in SETTINGS_SAFE_MODULES:
+        __import__(m)
 
-    def test_modules_that_shouldnt_load_settings(self):
-        sys.modules.pop("scrapy.conf", None)
-        for m in self.SETTINGS_SAFE_MODULES:
-            __import__(m)
-
-            self.assertNotIn(
-                "scrapy.conf", sys.modules, f"Module {m!r} must not cause the scrapy.conf module to be loaded"
-            )
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert "scrapy.conf" not in sys.modules, f"Module {m!r} must not cause the scrapy.conf module to be loaded"
