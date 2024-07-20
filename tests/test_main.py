9f27aa172bf15bd74f366def78b6fddf239b7d14
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-07-19 23:06:03 -0400

    test: Add tests for CLI changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,27 +8,31 @@ from scrapyd.__main__ import main
 __version__ = pkgutil.get_data(__package__, "../scrapyd/VERSION").decode("ascii").strip()
 
 
-def test_print_version(capsys, monkeypatch):
+def test_version(capsys, monkeypatch):
     monkeypatch.setattr(sys, "argv", ["scrapyd", "junk", "--version", "junk"])
     main()
 
     assert capsys.readouterr().out == f"Scrapyd {__version__}\n"
 
 
-def test_print_v(capsys, monkeypatch):
+def test_v(capsys, monkeypatch):
     monkeypatch.setattr(sys, "argv", ["scrapyd", "junk", "-v", "junk"])
     main()
 
     assert capsys.readouterr().out == f"Scrapyd {__version__}\n"
 
 
-def test_twisted_options(capsys, monkeypatch):
-    """
-    Test that the twisted options are correctly parsed.
-    """
+def test_help(capsys, monkeypatch):
     monkeypatch.setattr(sys, "argv", ["scrapyd", "--help"])
 
     with pytest.raises(SystemExit):
         main()
 
-    assert "twistd" in capsys.readouterr().out
+    out = capsys.readouterr().out
+
+    assert out.startswith("Usage: scrapyd [options]\n")
+    assert "--nodaemon" not in out
+    assert "python" not in out
+    assert "rundir" not in out
+    assert "ftp" not in out
+    assert "Commands:" not in out
